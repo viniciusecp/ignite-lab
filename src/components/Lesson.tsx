48a87055698b64721a6b847d5e34c0520c032b16
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2022-06-26 15:39:27 -0300

    Finished class 03

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react';
 import { isPast, format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { Link } from 'react-router-dom';
 
 interface LessonProps {
   title: string;
@@ -16,12 +17,12 @@ export function Lesson({title, slug, availableAt, type}: LessonProps) {
   });
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${slug}`} className="group">
       <span className="text-gray-300">
         {availableDateFormatted}
       </span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -33,8 +34,7 @@ export function Lesson({title, slug, availableAt, type}: LessonProps) {
               <Lock size={20} />
               Em breve
             </span>
-          )}
-          
+          )}          
 
           <span className="text-xs rounded border border-green-300 py-[0.125rem] px-2 text-white font-bold">
             {type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
@@ -45,6 +45,6 @@ export function Lesson({title, slug, availableAt, type}: LessonProps) {
           {title}
         </strong>
       </div>
-    </a>
+    </Link>
   );
 }
